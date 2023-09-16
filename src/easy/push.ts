0ddf1ccdb3c1ece1ac2bc8285c6b2fd06a51ce3e
danielwalters <waltersdaniel96@gmail.com>
2023-09-16 17:26:18 +1000

    Add Push

---
+++ src/easy/push.ts
@@ -0,0 +1,11 @@
+import { Equal, Expect } from "../utils";
+
+//type
+type Push<T extends any[], U> = [...T, U];
+
+// tests
+type cases = [
+  Expect<Equal<Push<[], 1>, [1]>>,
+  Expect<Equal<Push<[1, 2], "3">, [1, 2, "3"]>>,
+  Expect<Equal<Push<["1", 2, "3"], boolean>, ["1", 2, "3", boolean]>>
+];
