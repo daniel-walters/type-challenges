81c938d3e8123ae6bd1e9520e8e7a71072efcabd
danielwalters <waltersdaniel96@gmail.com>
2023-09-16 16:02:53 +1000

    Add Exclude

---
+++ src/easy/exclude.ts
@@ -0,0 +1,13 @@
+import { Equal, Expect } from "../utils";
+
+// type
+type MyExclude<T, U> = T extends U ? never : T;
+
+// tests
+type cases = [
+  Expect<Equal<MyExclude<"a" | "b" | "c", "a">, "b" | "c">>,
+  Expect<Equal<MyExclude<"a" | "b" | "c", "a" | "b">, "c">>,
+  Expect<
+    Equal<MyExclude<string | number | (() => void), Function>, string | number>
+  >
+];
