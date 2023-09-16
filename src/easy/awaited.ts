f15bf00a399faa8fabb2759e76177f74506fc409
danielwalters <waltersdaniel96@gmail.com>
2023-09-16 16:02:50 +1000

    Add Awaited

---
+++ src/easy/awaited.ts
@@ -0,0 +1,26 @@
+import { Equal, Expect } from "../utils";
+
+// type
+type MyAwaited<T extends PromiseLike<any>> = T extends PromiseLike<infer U>
+  ? U extends PromiseLike<any>
+    ? MyAwaited<U>
+    : U
+  : never;
+
+// tests
+type X = Promise<string>;
+type Y = Promise<{ field: number }>;
+type Z = Promise<Promise<string | number>>;
+type Z1 = Promise<Promise<Promise<string | boolean>>>;
+type T = { then: (onfulfilled: (arg: number) => any) => any };
+
+type cases = [
+  Expect<Equal<MyAwaited<X>, string>>,
+  Expect<Equal<MyAwaited<Y>, { field: number }>>,
+  Expect<Equal<MyAwaited<Z>, string | number>>,
+  Expect<Equal<MyAwaited<Z1>, string | boolean>>,
+  Expect<Equal<MyAwaited<T>, number>>
+];
+
+// @ts-expect-error
+type error = MyAwaited<number>;
