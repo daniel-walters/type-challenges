60217e9e438731f5ec51f412a552879c1fbbccd7
danielwalters <waltersdaniel96@gmail.com>
2023-09-18 16:03:48 +1000

    Add Flatten

---
+++ src/medium/flatten.ts
@@ -0,0 +1,28 @@
+import { Equal, Expect } from "../utils";
+
+// type
+type Flatten<T extends any[], Out extends any[] = []> = T extends [
+  infer E,
+  ...infer Rest
+]
+  ? E extends any[]
+    ? Flatten<[...E, ...Rest], Out>
+    : Flatten<Rest, [...Out, E]>
+  : Out;
+
+// tests
+type cases = [
+  Expect<Equal<Flatten<[]>, []>>,
+  Expect<Equal<Flatten<[1, 2, 3, 4]>, [1, 2, 3, 4]>>,
+  Expect<Equal<Flatten<[1, [2]]>, [1, 2]>>,
+  Expect<Equal<Flatten<[1, 2, [3, 4], [[[5]]]]>, [1, 2, 3, 4, 5]>>,
+  Expect<
+    Equal<
+      Flatten<[{ foo: "bar"; 2: 10 }, "foobar"]>,
+      [{ foo: "bar"; 2: 10 }, "foobar"]
+    >
+  >
+];
+
+// @ts-expect-error
+type error = Flatten<"1">;
