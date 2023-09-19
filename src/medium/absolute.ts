6262d90b3647d05a6c8c0421f60db48b3f6e3088
danielwalters <waltersdaniel96@gmail.com>
2023-09-19 11:30:03 +1000

    Add Absolute

---
+++ src/medium/absolute.ts
@@ -0,0 +1,22 @@
+import { Equal, Expect } from "../utils";
+
+// test
+type Absolute<T extends number | string | bigint> = T extends string
+  ? T extends `-${infer N}`
+    ? N
+    : T
+  : Absolute<`${T}`>;
+
+// types
+type cases = [
+  Expect<Equal<Absolute<0>, "0">>,
+  Expect<Equal<Absolute<-0>, "0">>,
+  Expect<Equal<Absolute<10>, "10">>,
+  Expect<Equal<Absolute<-5>, "5">>,
+  Expect<Equal<Absolute<"0">, "0">>,
+  Expect<Equal<Absolute<"-0">, "0">>,
+  Expect<Equal<Absolute<"10">, "10">>,
+  Expect<Equal<Absolute<"-5">, "5">>,
+  Expect<Equal<Absolute<-1_000_000n>, "1000000">>,
+  Expect<Equal<Absolute<9_999n>, "9999">>
+];
