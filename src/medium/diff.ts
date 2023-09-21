f5d50a785d132d16202cd44cc5a21abee95d1fe4
danielwalters <waltersdaniel96@gmail.com>
2023-09-21 14:44:47 +1000

    Add Diff

---
+++ src/medium/diff.ts
@@ -0,0 +1,28 @@
+import { Equal, Expect } from "../utils";
+
+// type
+type Diff<O, O1> = {
+  [K in keyof (O & O1) as K extends keyof (O | O1) ? never : K]: (O & O1)[K];
+};
+
+// tests
+type Foo = {
+  name: string;
+  age: string;
+};
+type Bar = {
+  name: string;
+  age: string;
+  gender: number;
+};
+type Coo = {
+  name: string;
+  gender: number;
+};
+
+type cases = [
+  Expect<Equal<Diff<Foo, Bar>, { gender: number }>>,
+  Expect<Equal<Diff<Bar, Foo>, { gender: number }>>,
+  Expect<Equal<Diff<Foo, Coo>, { age: string; gender: number }>>,
+  Expect<Equal<Diff<Coo, Foo>, { age: string; gender: number }>>
+];
