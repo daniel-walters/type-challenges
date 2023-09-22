811839ce36e33734b31311ccc9c90b8a3d1bac18
danielwalters <waltersdaniel96@gmail.com>
2023-09-22 14:22:20 +1000

    Add Drop Char

---
+++ src/medium/drop_char.ts
@@ -0,0 +1,19 @@
+import { Equal, Expect } from "../utils";
+
+// type
+type DropChar<
+  S extends string,
+  C extends string
+> = S extends `${infer L}${C}${infer R}` ? DropChar<`${L}${R}`, C> : S;
+
+// tests
+type cases = [
+  // @ts-expect-error
+  Expect<Equal<DropChar<"butter fly!", "">, "butterfly!">>,
+  Expect<Equal<DropChar<"butter fly!", " ">, "butterfly!">>,
+  Expect<Equal<DropChar<"butter fly!", "!">, "butter fly">>,
+  Expect<Equal<DropChar<"    butter fly!        ", " ">, "butterfly!">>,
+  Expect<Equal<DropChar<" b u t t e r f l y ! ", " ">, "butterfly!">>,
+  Expect<Equal<DropChar<" b u t t e r f l y ! ", "b">, "  u t t e r f l y ! ">>,
+  Expect<Equal<DropChar<" b u t t e r f l y ! ", "t">, " b u   e r f l y ! ">>
+];
