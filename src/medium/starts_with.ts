66f701a798ef348e9b4262ce526cd228449409bc
danielwalters <waltersdaniel96@gmail.com>
2023-09-22 14:22:26 +1000

    Add Starts With

---
+++ src/medium/starts_with.ts
@@ -0,0 +1,17 @@
+import { Equal, Expect } from "../utils";
+
+// type
+type StartsWith<T extends string, U extends string> = T extends `${U}${infer _}`
+  ? true
+  : false;
+
+// tests
+type cases = [
+  Expect<Equal<StartsWith<"abc", "ac">, false>>,
+  Expect<Equal<StartsWith<"abc", "ab">, true>>,
+  Expect<Equal<StartsWith<"abc", "abc">, true>>,
+  Expect<Equal<StartsWith<"abc", "abcd">, false>>,
+  Expect<Equal<StartsWith<"abc", "">, true>>,
+  Expect<Equal<StartsWith<"abc", " ">, false>>,
+  Expect<Equal<StartsWith<"", "">, true>>
+];
