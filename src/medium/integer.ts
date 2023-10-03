6889352a724028d24d1bdc2b8b6896d64aa9d7d9
danielwalters <waltersdaniel96@gmail.com>
2023-10-03 12:14:16 +1100

    Add Integer

---
+++ src/medium/integer.ts
@@ -0,0 +1,24 @@
+import { Equal, Expect } from "../utils";
+
+// type
+type Integer<T extends number> = number extends T
+  ? never
+  : `${T}` extends `${any}.${any}`
+  ? never
+  : T;
+
+// tests
+let x = 1;
+let y = 1 as const;
+
+type cases1 = [
+  Expect<Equal<Integer<1>, 1>>,
+  Expect<Equal<Integer<1.1>, never>>,
+  Expect<Equal<Integer<1.0>, 1>>,
+  Expect<Equal<Integer<1.0>, 1>>,
+  Expect<Equal<Integer<0.5>, never>>,
+  Expect<Equal<Integer<28.0>, 28>>,
+  Expect<Equal<Integer<28.101>, never>>,
+  Expect<Equal<Integer<typeof x>, never>>,
+  Expect<Equal<Integer<typeof y>, 1>>
+];
